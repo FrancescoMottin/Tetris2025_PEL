b461632b7cf1fa5ea333b32db4678c182e251eef
FrancescoMottin <mottinfg@gmail.com>
2025-07-28 14:31:44 +0200

    modifica operator<<

--- tetris.cpp
+++ tetris.cpp
@@ -1017,7 +1017,7 @@ std::istream& operator>>(std::istream& is, piece& p)
 std::ostream& operator<<(std::ostream& os, piece const& p)  //empty(i,j,s) and full(i,j,s) are useful to write the piece is the recursive format to an output stream
 {
     os << p.side() << " " << p.color() << " ";
-    output_grid_rec(os, p , p.side(), 0, 0);
+    output_grid_rec(os, p, p.side(), 0, 0);
     /*
     if(p.empty() == true) os << "[]";
     else
