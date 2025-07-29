a094dc55140a58c9a91c50924adb99688e926c15
FrancescoMottin <mottinfg@gmail.com>
2025-07-29 15:42:04 +0200

    Possibile fix per invalid write/read in cut_row()

--- tetris.cpp
+++ tetris.cpp
@@ -351,13 +351,13 @@ void piece::cut_row(uint32_t i)
 
     //Logica per lo scorrimento delle righe verso il basso (gravità interna al pezzo).
     // Y=0 è la riga superiore del pezzo, Y cresce verso il basso.
-    for(uint32_t r = i; r < m_side - 1; r++)
+    for(uint32_t r = i; r > 0 ; r--)    //for(uint32_t r = i; r < m_side - 1; r++)
         for(uint32_t c = 0; c < m_side; c++)    
             m_grid[r][c] = m_grid[r+1][c];
 
     //la riga 0 (la più in alto) conterrà una copia della sua versione originale. Deve essere svuotata
     for(uint32_t c = 0; c < m_side; c++)
-        m_grid[m_side-1][c] = false;
+        m_grid[0][c] = false;        //m_grid[m_side-1][c] = false;
 }
 
 uint32_t piece::side() const { return m_side; }
