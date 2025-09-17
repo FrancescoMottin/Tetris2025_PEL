6eb7317c37505ca6d68f8a84a113096d891e6768
FrancescoMottin <mottinfg@gmail.com>
2025-09-17 18:32:06 +0200

    rollback insert

--- tetris.cpp
+++ tetris.cpp
@@ -468,15 +468,8 @@ bool tetris::operator==(tetris const& rhs) const
 }
 bool tetris::operator!=(tetris const& rhs) const { return !operator==(rhs);}
 
-/*
-//Nota che il controllo se il row sia completamente usato tocca a questa funzione, cut_row() cancella solo la riga incriminata
 void tetris::insert(piece const& p, int x) //Gestisce il campo di gioco
 {
-    //Se il il piece è più grande del campo di gioco? throw_exception
-    if(m_width == 0 || m_height == 0) throw tetris_exception("ERROR! - insert(piece const& p, int x) - Il tabellone non è stato inizializzato con dimensioni valide.");
-    if(x >= (int) m_width) throw tetris_exception("ERROR! - insert(piece const& p, int x) - Pezzo più grande del campo di gioco.");
-    
-    //1. Trovare posizione di caduta
     int pos_x = 0;
     int pos_y = -1; //-1 :posizione non trovata
     bool pos_found = false;
@@ -500,50 +493,9 @@ void tetris::insert(piece const& p, int x) //Gestisce il campo di gioco
         }
         if(pos_found) break ;
     }
-    
+
     //Si attiva troppo facilmente, o la logica si attiva troppo facilmente o non si trova il posizione facilmente
     if(!pos_found)  throw tetris_exception("GAME OVER! - insert(piece const& p, int x) - Non possiamo inserire altri pezzi!"); 
-
-    try { add(p, pos_x, pos_y); }
-    catch (const tetris_exception& e) { throw tetris_exception(e.what()); }
-}
-*/
-
-void tetris::insert(piece const& p, int x) //Gestisce il campo di gioco
-{
-    if(m_width == 0 || m_height == 0) throw tetris_exception("ERROR! - insert(piece const& p, int x) - Il tabellone non è stato inizializzato con dimensioni valide.");
-    if( /*x < 0 ||*/ x >= (int) m_width) throw tetris_exception("ERROR! - insert(piece const& p, int x) - Pezzo più grande del campo di gioco.");
-    //if((int) p.side() > (int) m_height) throw tetris_exception("ERROR! - insert(piece const& p, int x) -  Pezzo più alto del campo di gioco.");
-    //Se il il piece è più grande del campo di gioco? throw_exception
-
-    //1. Trovare posizione di caduta
-    //int pos_y = 0;
-    bool pos_found = false;
-    int bottom_filled_row = -1;
-
-    for(int r = p.side()-1; r >= 0; r--) 
-        for(int c = 0; c < p.side(); c++)
-            if(p(r,c) && r > bottom_filled_row) bottom_filled_row = r;
-    
-   int pos_y = -1;  // -1 significa che non abbiamo trovato posizione
-    for(int y = m_height - p.side(); y >= 0; y--)
-    {
-        bool can_place;
-        try { can_place = containment(p, x, y); } 
-        catch(const tetris_exception& e) { throw tetris_exception(e.what());}
-
-        if(can_place)
-        {
-            // posizioniamo il bottom-most filled row del pezzo su questa y
-            pos_y = y + (p.side() - 1 - bottom_filled_row);
-            break;
-        }
-    }
-    if(pos_y == -1) throw tetris_exception("GAME OVER! - insert(piece const& p, int x) - Non possiamo inserire altri pezzi!");
-
-
-    //Si attiva troppo facilmente, o la logica si attiva troppo facilmente o non si trova il posizione facilmente
-    //if(!pos_found)  throw tetris_exception("GAME OVER! - insert(piece const& p, int x) - Non possiamo inserire altri pezzi!"); 
 
     try { add(p,x, pos_y); }
     catch (const tetris_exception& e) { throw tetris_exception(e.what()); }
