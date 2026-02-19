69661b564deaa369c7f6a5a3355deb40c1b09fb4
FrancescoMottin <mottinfg@gmail.com>
2026-02-19 20:00:56 +0100

    elimina commenti pieces_number in operator<< e operator>>

--- tetris.cpp
+++ tetris.cpp
@@ -1050,28 +1050,6 @@ std::istream& operator>>(std::istream& is, tetris& t)
         }
     }
 
-    /*
-    uint32_t num_pieces;
-    is >> std::skipws >> num_pieces;
-    CHECK_ERR(is.fail(), "ERROR! - operator>>(std::istream& is, tetris& t) - Formato input invalido (lettura fallita).");
-
-    for(uint32_t i = 0; i < num_pieces; i++)
-    {   
-        piece p_data;
-        int x = 0;
-        int y = 0;
-
-        is >> std::skipws >> p_data;
-        is >> std::skipws >> x >> y;
-        
-        CHECK_ERR(is.fail(), "ERROR! - operator>>(std::istream& is, tetris& t) - Formato input invalido (lettura fallita).");
-
-        try { temp_t.add(p_data, x, y); }  
-        catch(const std::bad_alloc& e) { CHECK_ERR(true, "ERROR! - operator>>(std::istream& is, tetris& t) - Errore di allocazione nella lista (causa add)"); }
-        catch (const tetris_exception& e) { CHECK_ERR(true, e.what()); }
-    }
-    */
-
     t = std::move(temp_t);
     return is;
 }
@@ -1079,13 +1057,6 @@ std::istream& operator>>(std::istream& is, tetris& t)
 std::ostream& operator<<(std::ostream& os, tetris const& t)
 {
     os << t.score() << " " << t.width() << " " << t.height() << std::endl;  //Dimensioni e Punteggio
-
-    //PIECE_COUNT NON STRETTAMENTE RICHIESTO
-    /*
-    uint32_t piece_count = 0;
-    for(auto it = t.begin(); it != t.end(); ++it)  { piece_count++; }
-    os << piece_count << std::endl;                     //Numero pezzi
-    */
 
     for(auto it = t.begin(); it != t.end(); ++it)
         os << it->p << " " << it->x << " " << it->y << std::endl;  //Piece, X e Y
